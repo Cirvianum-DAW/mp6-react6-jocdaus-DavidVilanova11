257d81d0521ca308ea35bd01c4719b639004b785
daw25 <navjashan.singh@cirvianum.cat>
2024-05-14 17:52:27 +0200

    Refactor LuckyN component to include a button for rolling the dice

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 // import LuckyN from './LuckyN'
 import Dice from './Dice'
 import { getTirades, sum } from './utils'
+import LuckyN from './LuckyN'
 
 
 export const App = () => {
-    const prova = [3, 4]
   return (
 <>
     {/* <LuckyN /> */}
-    <Dice dice={prova} />
+    <LuckyN numDaus={2} suma={2} />
 </>
   )
 }
--- src/LuckyN.jsx
+++ src/LuckyN.jsx
@@ -6,10 +6,28 @@ import { getTirades, sum } from './utils'
 const LuckyN = ({ numDaus, suma }) => {
     const [dice, setDice] = useState(getTirades(numDaus));
 
+  const handleClick = () => {
+    setDice(getTirades(numDaus));
+  }
+
     const isWinner = sum(dice) === suma;
   return (
-    <div>LuckyN</div>
-  )
+    <main>
+      <h1 className='mt-3 text-center text-xl text-blue-500'>
+        Prova Sort! La suma ha de ser {suma}
+      </h1>
+      
+      <Dice dice={dice} />
+      (isWinner ? <h2 className='text-center text-2xl text-green-500'>HAS GUANYAT!</h2> : null)
+      <button
+        className='block mx-auto mt-3 p-2 bg-blue-500 text-white rounded hover:bg-blue-700'
+        onClick={() => setDice(getTirades(numDaus))}
+      >
+        Tira els daus
+      </button>
+    </main>
+  );
+
 }
 
 export default LuckyN
